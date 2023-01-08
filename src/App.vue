448f81ec8361d9581ad3922c5b52254fc97d3d1f
SSQLQIANBB <44046500+SSQLQIANBB@users.noreply.github.com>
2023-01-08 14:25:09 +0800

    feat: 环境变量与模式

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,19 @@ import inline from './styles/inline.module.css?inline'; // 禁用css
 
 console.log('url-IMG', Img)
 
+// 环境变量
+// import.meta.env.MODE: {string} 应用运行的模式。
+
+// import.meta.env.BASE_URL: {string} 部署应用时的基本 URL。他由base 配置项决定。
+
+// import.meta.env.PROD: {boolean} 应用是否运行在生产环境。
+
+// import.meta.env.DEV: {boolean} 应用是否运行在开发环境 (永远与 import.meta.env.PROD相反)。
+
+// import.meta.env.SSR: {boolean} 应用是否运行在 server 上。
+
+
+
 /**
  * 功能
  */
@@ -58,6 +71,7 @@ let admin = {
 };
 console.log('name', admin.name)
 
+console.log(import.meta.env.VITE_BASIC_URL)
 </script>
 
 <template>
